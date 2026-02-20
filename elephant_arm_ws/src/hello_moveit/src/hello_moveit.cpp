2eb530e643df61192987551f2b4100ec323a9a15
Sophie Green <sophie.green-5@postgrad.manchester.ac.uk>
2026-02-20 12:09:07 +0000

    hello_moveit node created, attempting to inverse kinematics working to get current robot pose

--- elephant_arm_ws/src/hello_moveit/src/hello_moveit.cpp
+++ elephant_arm_ws/src/hello_moveit/src/hello_moveit.cpp
@@ -1,6 +1,7 @@
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.hpp>
+#include <moveit/robot_state/robot_state.hpp>
 
 int main(int argc, char * argv[]){
     // Initialize ROS 2
@@ -17,6 +18,9 @@ int main(int argc, char * argv[]){
     using moveit::planning_interface::MoveGroupInterface;
     auto move_group_interface = MoveGroupInterface(node, "arm_with_gripper");
 
+    move_group_interface.startStateMonitor();
+    rclcpp::sleep_for(std::chrono::seconds(3));
+
     // Set a target Pose
     auto const target_pose = []{
         geometry_msgs::msg::Pose msg;
@@ -28,18 +32,37 @@ int main(int argc, char * argv[]){
     }();
     move_group_interface.setPoseTarget(target_pose);
 
-    //Create a plan to that target pose
-    auto const [success, plan] = [&move_group_interface]{
-        moveit::planning_interface::MoveGroupInterface::Plan msg;
-        auto const ok = static_cast<bool>(move_group_interface.plan(msg));
-        return std::make_pair(ok,msg);
-    }();
+    // Check if the target is reachable
+    moveit::core::RobotStatePtr current_state = move_group_interface.getCurrentState();
 
-    // Execute plan
-    if (success){
-        move_group_interface.execute(plan);
-    } else{
-        RCLCPP_ERROR(logger, "Planning Failed");
+    if (!current_state){
+        RCLCPP_ERROR(logger, "FATAl failes to get robot state");
+        return 1;
+    }
+
+    const moveit::core::JointModelGroup* joint_model_group = current_state->getJointModelGroup("arm_with_gripper");
+
+    bool found_ik = current_state->setFromIK(joint_model_group, target_pose);
+
+    if(found_ik){
+        RCLCPP_INFO(logger, "Point is reachable");
+
+        //Create a plan to that target pose
+        auto const [success, plan] = [&move_group_interface]{
+            moveit::planning_interface::MoveGroupInterface::Plan msg;
+            auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+            return std::make_pair(ok,msg);
+        }();
+
+        // Execute plan
+        if (success){
+            move_group_interface.execute(plan);
+        } else{
+            RCLCPP_ERROR(logger, "Planning Failed");
+    }
+    }
+    else{
+        RCLCPP_ERROR(logger, "Point is outside the workspace");
     }
 
     rclcpp::shutdown();
